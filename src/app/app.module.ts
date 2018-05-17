bb3c34085315035a3163097669acc2b5fde7b688
chikakow <chikakow@gmail.com>
2018-05-17 11:45:53 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material';
+import { ProductModule } from './product/product.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { ProductListEffects } from './product/effects/product-list.effects';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/products'},
+  {
+    path: 'products',
+    loadChildren: './product/product.module#ProductModule',
+  },
+]
 
 
 @NgModule({
@@ -10,7 +25,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    ProductModule,
+    RouterModule.forRoot(routes),
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({maxAge: 50})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,15 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+
+/* SystemJS module definition */
+// declare const module: NodeModule;
+// interface NodeModule {
+//   id: string;
+// }
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
